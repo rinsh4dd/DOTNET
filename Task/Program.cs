43c2a0bd620cceb895f596d981138b455d5f62be
rinshad7518 <rinshadcontacts@gmail.com>
2025-08-05 16:07:18 +0530

    TASK@

--- Task/Program.cs
+++ Task/Program.cs
@@ -30,39 +30,190 @@
 
 
 //Task 2
+
+//using System;
+
+//class PrimeChecker
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Enter a number: ");
+//        int number = int.Parse(Console.ReadLine());
+
+//        bool isPrime = true;
+
+//        if (number <= 1)
+//        {
+//            isPrime = false;
+//        }
+//        else
+//        {
+
+//            for (int i = 2; i <= number / 2; i++)
+//            {
+//                if (number % i == 0)
+//                {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//        }
+
+//        if (isPrime)
+//            Console.WriteLine($"{number} is a Prime number.");
+//        else
+//            Console.WriteLine($"{number} is NOT a Prime number.");
+
+//        Console.ReadKey();
+//    }
+//}
+
+
+//Task Day2 task1
+
+//using System;
+//class oddOrEven
+//{
+//static void Main()
+//{
+
+//        Console.WriteLine("Enter a number");
+//        int num = Convert.ToInt32(Console.ReadLine());
+//        if (num % 2 == 0)
+//        {
+//            Console.WriteLine("NUMBER IS EVEN");
+//        }
+//        else
+//        {
+//            Console.WriteLine("NUMBER IS ODD");
+//        }
+
+//    }
+//}
+
+
+//Task Day2 task2
+
+//using System;
+
+//class PosNegZero
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter a Number");
+//        int num = Convert.ToInt32(Console.ReadLine());
+
+
+//        switch (num)
+//        {
+//            case > 0:
+//                Console.WriteLine("Number is Positive");
+//                break;
+//            case <0:
+//                Console.WriteLine("Number is Negative");
+//                break;
+//            case  0:
+//                Console.WriteLine("Number is zero");
+//                break;
+//        }
+
+//    }
+//}
+
+
+//Task Day2 task3
+
+//using System;
+
+//class Largest
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter First Number");
+
+//        int num1 = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Enter Second Number");
+//        int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+//        Console.WriteLine("Enter Third Number");
+//        int num3 = Convert.ToInt32(Console.ReadLine());
+
+//        if(num1>num2 && num1 > num3)
+//        {
+//            Console.WriteLine($"Largest number is :{num1}");
+//        }
+//        else if (num2 > num1 && num2 > num3)
+//        {
+//            Console.WriteLine($"Largest number is :{num2}");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"Largest number is :{num3}");
+
+//        }
+
+
+//    }
+//}
+
+//Task Day2 task3
+
+//using System;
+//class LeapYear
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine( " Please Enter Year");
+//        int year = Convert.ToInt32(Console.ReadLine());
+
+//        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//        {
+//            Console.WriteLine("Leap Year");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Not a Leap Year");
+//        }
+
+//    }
+
+//}
+
+
+//Task Day2 task4
+
 using System;
-
-class PrimeChecker
+class Grade
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.Write("Enter a number: ");
-        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter MARK");
+        int Mark = Convert.ToInt32(Console.ReadLine());
 
-        bool isPrime = true;
-
-        if (number <= 1)
+        switch (Mark)
         {
-            isPrime = false;
-        }
-        else
-        {
-            // Loop from 2 to number-1
-            for (int i = 2; i <= number / 2; i++)
-            {
-                if (number % i == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
+            case int m when(m>=90&&m<=100):Console.WriteLine("S Grade");
+                break;
 
-        if (isPrime)
-            Console.WriteLine($"{number} is a Prime number.");
-        else
-            Console.WriteLine($"{number} is NOT a Prime number.");
-
-        Console.ReadKey(); // Pause the console window
+            case int m when (m >= 80 && m <= 90):
+                Console.WriteLine("A Grade");
+                break;
+            case int m when (m >= 70 && m <= 80):
+                Console.WriteLine("B Grade");
+                break;
+            case int m when (m >= 60 && m <= 70):
+                Console.WriteLine("C Grade");
+                break;
+            case int m when (m >= 50 && m <= 60):
+                Console.WriteLine("D Grade");
+                break;
+            case int m when (m >= 40 && m <= 50):
+                Console.WriteLine("E Grade");
+                break;
+            case int m when (m <40):
+                Console.WriteLine("Student has Failed!!");
+                break;
+        }
     }
 }
